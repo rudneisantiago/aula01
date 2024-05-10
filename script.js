517dcd5654c72321ba0974245176ef09e3c44c18
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-05-09 21:17:07 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// // declarando
+// var nome;
+// var idade;
+
+// // atribuindo
+// nome = "Um nome";
+// idade = 30;
+
+// // inicializando
+// var aula = "Aula 02";
+
+var numeroA = 1;
+var numeroB = 2;
+var numeroC = 3;
+
+//Soma de dois números
+var resultadoSoma = numeroA + numeroB;
+
+//Subtração de dois números
+var resultadoSubtracao = numeroB - numeroA;
+
+//Produto de dois números
+var resultadoProduto = numeroB * numeroC;
+
+//Divisão de dois números
+var resultadoDivisao = numeroC / numeroB;
+
+var textoA = "CODER";
+var textoB = "HOUSE";
+var branco = " ";
+
+//Concatenar textoA e texto B
+var resultadoA = textoA + textoB;
+
+//Concatenar textoB e 1
+var resultadoB = textoB + 1;
+
+//Concatenar textoA, branco e textoB
+var resultadoC = textoA + branco + textoB;
+
+var resultadoD = 10.5 + 10;
+
+var inserirNumero = prompt("Insira um número");
+
+console.log(inserirNumero);
+
+var resultado = parseInt(inserirNumero) + 10;
+
+alert(resultado);
